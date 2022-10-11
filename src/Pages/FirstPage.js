ad19987d402b3fd999fdc13020212df783ae7a5d
altamash2000 <altamash.shaikh@moreyeahs.in>
2022-10-11 15:16:53 +0530

    logout

--- src/Pages/FirstPage.js
+++ src/Pages/FirstPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const FirstPage = () => {
+  const logOut = () => {
+    localStorage.removeItem('auth')
+  }
   return (
     <div>
       first page
+      <button onClick={logOut} >LOGOUT</button>
     </div>
   )
 }
