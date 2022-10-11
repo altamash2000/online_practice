dad09d9751697fd809b251c7c20f133ce5db6130
Anjali459 <adubey@moreyeahs.in>
2022-10-11 10:04:09 +0530

    commit

--- src/auth/signup/index.js
+++ src/auth/signup/index.js
@@ -4,12 +4,7 @@ import { ThemeColors } from '../../theme/theme'
 export default function Signup() {
   return (
     <>
-    <div className='left-flex' style={{ backgroundColor: ThemeColors.light }}>
-
-    </div>
-    <div className='right-flex' style={{ backgroundColor: ThemeColors.primary }}>
-
-    </div>
+    Signup
   </>
   )
 }
