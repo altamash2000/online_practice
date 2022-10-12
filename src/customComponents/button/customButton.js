76ed3f29b64d249c446b815de2e573a06dcb703c
altamash2000 <altamash.shaikh@moreyeahs.in>
2022-10-12 13:05:01 +0530

    upload image

--- src/customComponents/button/customButton.js
+++ src/customComponents/button/customButton.js
@@ -1,29 +1,32 @@
 import { ThemeColors } from '../../theme/theme';
+import { Button } from '../customTextInput/indexCss';
 export default function CustomButton(props) {
-  const { title, onClick } = props;
+  const { title, onClick, width = '100%' } = props;
   // define your styles
   const styles = {
     container: {
-      textAlign: 'center',
+      display: 'flex',
+      justifyContent: 'center',
+      // textAlign: 'center',
       backgroundColor: ThemeColors.primary,
       borderRadius: 5,
-      height:`calc(100px - 50px)`,
-      width:"100%",
-      cursor:'pointer'
+      // height:`calc(100px - 50px)`,
+      width: width,
+      cursor: 'pointer',
+      padding: '0 15px 0 15px',
     },
     title: {
       color: ThemeColors.white,
-      fontSize:'20px',
-      lineHeight:'20px',
-      textAlign: 'center',
-      padding:15
+      fontSize: '20px',
+      // lineHeight:'20px',
+      // textAlign: 'center',
+      // padding:15
     },
   };
+
   return (
-    <div style={styles.container} onClick={() =>{onClick()}}>
-      <div>
-        <p style={styles.title}>{title}</p>
-      </div>
+    <div style={styles.container} onClick={() => { onClick() }}>
+      <Button className='btn' >{title}</Button>
     </div>
   )
 }
--- src/customComponents/customTextInput/indexCss.js
+++ src/customComponents/customTextInput/indexCss.js
@@ -24,6 +24,21 @@ export const InputLabel = styled.label`
   font-family:medium;
 `;
 
+export const Button = styled.button`
+background-color:  transparent;
+border: none;
+border-radius: 5px;
+color: white;
+padding: 15px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 20px;
+font-family:SemiBold;
+margin: 4px 2px;
+cursor: pointer;
+`
+
 export const formControl = {
   display: 'block',
   width: '95%',
@@ -36,24 +51,24 @@ export const formControl = {
   border: 'none',
 }
 export const formSelect = {
-    display:'block',
-    width:'100%',
-    fontSize:'1rem',
-    background:ThemeColors.inputbg,
-    textAlign:'center',
-    border:'none',
-    paddingTop:'0.9rem',
-    paddingBottom:'0.9rem',
-    paddingLeft:'1rem',
+  display: 'block',
+  width: '100%',
+  fontSize: '1rem',
+  background: ThemeColors.inputbg,
+  textAlign: 'center',
+  border: 'none',
+  paddingTop: '0.9rem',
+  paddingBottom: '0.9rem',
+  paddingLeft: '1rem',
 
-    // display: 'block',
-    // width: '95%',
-    // padding:'0.375rem 2.25rem 0.375rem 0.75rem',
-    // fontSize:' 1rem',
-    // lineHeight:' 2.4rem',
-    // backgroundClip: 'padding-box', 
-    // background:ThemeColors.inputbg,
-    // textAlign:'center',
-    // border: 'none',
-    // appearance: 'none',
+  // display: 'block',
+  // width: '95%',
+  // padding:'0.375rem 2.25rem 0.375rem 0.75rem',
+  // fontSize:' 1rem',
+  // lineHeight:' 2.4rem',
+  // backgroundClip: 'padding-box', 
+  // background:ThemeColors.inputbg,
+  // textAlign:'center',
+  // border: 'none',
+  // appearance: 'none',
 }
